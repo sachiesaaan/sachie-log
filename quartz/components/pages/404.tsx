a3446cca2fb8c788eea83d845c7454e3692cd311
standingflower0607 <tachibanaa1211@gmail.com>
2025-05-02 19:08:31 +0900

    update workflow

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -3,10 +3,8 @@ import { QuartzComponentConstructor } from "../types"
 function NotFound() {
   return (
     <article class="popover-hint">
-      <h2 class={`article-title`}>4️⃣0️⃣4️⃣</h2>
-      <blockquote>In computer network communications, the HTTP 404, 404 not found, 404, 404 error, page not found, or file not found error message is a hypertext transfer protocol (HTTP) standard response code, to indicate that the browser was able to communicate with a given server, but the server could not find what was requested. The error may also be used when a server does not wish to disclose whether it has the requested information.[1]
-
-        The website hosting server will typically generate a "404 Not Found" web page when a user attempts to follow a broken or dead link; hence the 404 error is one of the most recognizable errors encountered on the World Wide Web. - Wikipedia</blockquote>
+      <h2 class={`article-title`}>404</h2>
+      <blockquote>ページが存在しないか、非公開になっています。</blockquote>
     </article>
   )
 }
